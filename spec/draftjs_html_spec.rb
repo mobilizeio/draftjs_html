4632bf0883d135902886b2bfb5000b4e1dc8eaab
TJ Taylor <dugancathal@gmail.com>
2022-09-27 18:38:59 -0600

    Add a little DSL to make the JSON easier in specs

--- spec/draftjs_html_spec.rb
+++ spec/draftjs_html_spec.rb
@@ -6,14 +6,49 @@ RSpec.describe DraftjsHtml do
   end
 
   it 'generates valid HTML from the most basic of DraftJS' do
-    html = described_class.to_html({ 'blocks' => [{ 'text' => 'Hello world!' }], 'entityMap' => {} })
+    raw_draftjs = RawDraftJs.build do
+      text_block 'Hello world!'
+    end
+
+    html = described_class.to_html(raw_draftjs)
 
     expect(html).to eq '<p>Hello world!</p>'
   end
 
   it 'supports generating multiple blocks of plain text' do
-    html = described_class.to_html({ 'blocks' => [{ 'text' => 'Hello world!' }, { 'text' => 'Winter is coming.' }], 'entityMap' => {} })
+    raw_draftjs = RawDraftJs.build do
+      text_block 'Hello world!'
+      text_block 'Winter is coming.'
+    end
+
+    html = described_class.to_html(raw_draftjs)
 
     expect(html).to eq "<p>Hello world!</p>\n<p>Winter is coming.</p>"
+  end
+
+  private
+
+  class RawDraftJs
+    def self.build(&block)
+      instance = new
+      instance.instance_eval(&block)
+      instance.to_h
+    end
+
+    def initialize
+      @blocks = []
+      @entity_map = {}
+    end
+
+    def text_block(text)
+      @blocks << { 'text' => text }
+    end
+
+    def to_h
+      {
+        'blocks' => @blocks,
+        'entityMap' => @entity_map,
+      }
+    end
   end
 end
