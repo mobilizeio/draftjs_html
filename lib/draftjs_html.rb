e0465cff7583df3920fce00f271cef18bc9fb92c
TJ Taylor <dugancathal@gmail.com>
2022-09-27 18:41:56 -0600

    Get away from manually building HTML with string munging

--- lib/draftjs_html.rb
+++ lib/draftjs_html.rb
@@ -1,11 +1,20 @@
 # frozen_string_literal: true
 
 require_relative "draftjs_html/version"
+require 'nokogiri'
 
 module DraftjsHtml
   class Error < StandardError; end
 
   def self.to_html(raw_draftjs)
-    raw_draftjs['blocks'].map { "<p>#{_1['text']}</p>" }.join("\n")
+    document = Nokogiri::HTML::Builder.new do |html|
+      html.body do |body|
+        raw_draftjs['blocks'].each do |block|
+          body.p block['text']
+        end
+      end
+    end.to_html
+
+    Nokogiri::HTML(document).css('body').first.children.to_html.strip
   end
 end
