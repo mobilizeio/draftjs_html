490dcfbb3c02b98dc93904fa0e28121b1df2d0f2
TJ Taylor <dugancathal@gmail.com>
2022-09-27 18:01:27 -0600

    Initial commit - scaffold

---
+++ spec/draftjs_html_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe DraftjsHtml do
+  it "has a version number" do
+    expect(DraftjsHtml::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
