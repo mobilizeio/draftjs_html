f59f3700211ecf033645153359443b3ac28baad8
TJ Taylor <tj@mobilize.io>
2022-10-12 17:23:46 -0600

    Improve diff output from eq_ignoring_whitespace

--- spec/support/whitespace_helpers.rb
+++ spec/support/whitespace_helpers.rb
@@ -1,10 +1,24 @@
 RSpec::Matchers.define :eq_ignoring_whitespace do |expected|
   match do |actual|
     @actual = actual.gsub(/[[:space:]]/, '')
-    @expected_as_array = [expected.gsub(/[[:space:]]/, '')]
+    @expected = expected.gsub(/[[:space:]]/, '')
+    @expected_as_array = [@expected]
 
     values_match?(@expected_as_array.first, @actual)
   end
 
   diffable
+
+  failure_message do |actual|
+    message = "expected that #{actual} would match #{@expected}"
+    message += "\nDiff:" + differ.diff_as_string(actual, @expected)
+    message
+  end
+
+  def differ
+    RSpec::Support::Differ.new(
+      :object_preparer => lambda { |object| RSpec::Matchers::Composable.surface_descriptions_in(object) },
+      :color => RSpec::Matchers.configuration.color?
+    )
+  end
 end
