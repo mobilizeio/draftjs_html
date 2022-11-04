4cf1e3fdc11700a66add0532b918ece78d221ee1
TJ Taylor <tj@mobilize.io>
2022-11-04 00:25:55 -0600

    Remove test with unfortunate name
    This is actually covered in the primary FromHtml spec

--- spec/draftjs_html/from_html/reciprocal_spec.rb
+++ spec/draftjs_html/from_html/reciprocal_spec.rb
@@ -19,12 +19,4 @@ RSpec.describe DraftjsHtml::FromHtml, 'Start->ToHtml->FromHtml == Start' do
     expect(DraftjsHtml.to_html(raw_draftjs)).to eq '<p>Hi, <a href="https://example.com/kittens">a</a></p>'
     expect(DraftjsHtml.from_html(DraftjsHtml.to_html(raw_draftjs))).to eq_raw_draftjs_ignoring_keys raw_draftjs
   end
-
-  it 'works on the case I actually care about' do
-    html = '<p>Hi <a id="mob-widget-1"></a>, How are you?</p>'
-    expect(DraftjsHtml.from_html(html)).to eq_raw_draftjs {
-      text_block 'Hi  , How are you?'
-      apply_entity 'LINK', 3..3, data: { id: 'mob-widget-1' }, mutability: 'MUTABLE'
-    }
-  end
 end
