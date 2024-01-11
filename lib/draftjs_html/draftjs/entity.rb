94cf52c8cb31d57e53d4145e8ca559293d99a0ab
TJ Taylor <tj@mobilize.io>
2024-01-11 11:12:50 -0700

    Expose entity key on struct (#8)
    We've found in a few instances that having to pass around the
    `EntityMap` key values in addition to the `Entity` struct causes a fair
    amount of hassle. This aims to make that less irritating by directly
    including the `key` value in the `Entity`

--- lib/draftjs_html/draftjs/entity.rb
+++ lib/draftjs_html/draftjs/entity.rb
@@ -2,9 +2,10 @@
 
 module DraftjsHtml
   module Draftjs
-    Entity = Struct.new(:type, :mutability, :data, keyword_init: true) do
-      def self.parse(raw)
+    Entity = Struct.new(:type, :mutability, :data, :key, keyword_init: true) do
+      def self.parse(raw, key: nil)
         new(
+          key: key,
           type: raw['type'],
           mutability: raw['mutability'],
           data: raw['data'],
